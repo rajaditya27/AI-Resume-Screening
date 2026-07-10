e08191a8f99063629d2de351d67a2c05e75c040c
rajaditya27 <adityarajthakur1112@gmail.com>
2026-07-10 12:06:11 +0530

    Day-4 : Created Users table using SQLAlchemy ORM

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from app.models.user import User
+
+from app.database.database import engine,Base
 
 app = FastAPI(
     title="AI Resume Screening System",
     description="An AI-powered ATS Resume Screening Application",
     version="1.0.0"
 )
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def home():
