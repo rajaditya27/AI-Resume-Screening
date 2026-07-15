6d559b1304c608b6c9afe8b38afa4da74decff73
rajaditya27 <adityarajthakur1112@gmail.com>
2026-07-15 10:05:08 +0530

    Day - 7: Add duplicate email validation and secure password hashing

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,26 +1,34 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter,HTTPException,status
 from app.schemas.user_schema import UserCreate
 from sqlalchemy.orm import Session
 from app.database.database import get_db
 from app.schemas.user_schema import UserCreate
-from app.models.user import User        
+from app.models.user import User   
+from app.utils.security import hash_password     
 
 
 router = APIRouter()
-@router.post("/register")
+# @router.post("/register")
 # def register(user: UserCreate):
 #     return{
 #         "message": "User registration API Working",
 #         "name": user.name,
 #         "email": user.email
 #     }
-@router.post("/register")
+@router.post("/register",status_code=status.HTTP_201_CREATED)
 def register(user: UserCreate,db: Session = Depends(get_db)):
+    # Step1: Check duplicate email
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=HTTP_409_CONFLICT, detail="Email already Exists")     
+    hashed_password = hash_password(user.password)
+    # Step 2: Create user object
     new_user=User(
         name=user.name,
         email=user.email,
-        password=user.password
+        password=hashed_password
     )
+    # Step 3: Save to Database
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
---
+++ backend/app/utils/security.py
@@ -0,0 +1,8 @@
+import bcrypt
+
+def hash_password(password : str) -> str:
+    hashed_password = bcrypt.hashpw(
+        password.encode('utf-8'), 
+        bcrypt.gensalt()
+        )
+    return hashed_password.decode('utf-8')
