28e7ff6d356384366d4bdc8217f4fe2a12669c5b
rajaditya27 <adityarajthakur1112@gmail.com>
2026-07-22 15:15:17 +0530

    Day 11: Improve JWT authentication with exception handling

--- backend/app/utils/auth.py
+++ backend/app/utils/auth.py
@@ -29,27 +29,23 @@ def get_current_user(
     db: Session = Depends(get_db)
     ):
     
-    # credentials_exception = HTTPException(
-    #     status_code=status.HTTP_401_UNAUTHORIZED,
-    #     detail="Could not validate credentials",
-    #     headers={"WWW-Authenticate": "Bearer"},
-    # )
-
-    payload = jwt.decode(
-        token, 
-        SECRET_KEY, 
-        algorithms=[ALGORITHM]
-        )
-    email = payload.get("sub")
-    if email is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials"
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
     )
+    try:
+        payload = jwt.decode(
+            token, 
+            SECRET_KEY, 
+            algorithms=[ALGORITHM]
+            )
+        email = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
     current_user = db.query(User).filter(User.email == email).first()
     if current_user is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="user not found"
-        )
+        raise credentials_exception
     return current_user
